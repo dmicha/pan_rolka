2cd777c253ae174ab410fb0f113caec7dce005af
dmicha <d.michalak94@gmail.com>
2020-12-01 17:10:29 +0100

    form fix v7

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -5,29 +5,29 @@ import axios from "axios"
 import PageInfo from "../components/PageInfo/PageInfo"
 import Button from "../components/Button/Button"
 import ReCAPTCHA from "react-google-recaptcha"
+import style from "./contact.module.css"
+// const StyledInput = styled.input`
+//   display: block;
+//   border: 2px solid black;
+//   background: none;
+//   font-family: Montserrat;
+//   font-size: 20px;
+//   height: ${({ as }) => (as ? "200px" : "auto")};
+//   width: ${({ as }) => (as ? "500px" : "300px")};
+//   margin-bottom: ${({ as }) => as && "40px"};
+// `
 
-const StyledInput = styled.input`
-  display: block;
-  border: 2px solid black;
-  background: none;
-  font-family: Montserrat;
-  font-size: 20px;
-  height: ${({ as }) => (as ? "200px" : "auto")};
-  width: ${({ as }) => (as ? "500px" : "300px")};
-  margin-bottom: ${({ as }) => as && "40px"};
-`
-
-const StyledLabel = styled.label`
-  margin: 30px 0 10px;
-  display: block;
-  font-size: 14px;
-  font-weight: bold;
-  font-family: Montserrat;
-`
+// const StyledLabel = styled.label`
+//   margin: 30px 0 10px;
+//   display: block;
+//   font-size: 14px;
+//   font-weight: bold;
+//   font-family: Montserrat;
+// `
 
 const pageData = {
   title: "Kontakt",
-  paragraph: `Zapraszamy do zadawania pytań. Napewno odpowiemy.`,
+  paragraph: "Zapraszamy do zadawania pytań. Napewno odpowiemy.",
 }
 
 const ContactPage = () => (
@@ -40,12 +40,18 @@ const ContactPage = () => (
       data-netlify="true"
       data-netlify-honeypot="bot-field"
     >
-      <p class="hidden">
-        <input name="bot-field" />
+      <p className="hidden" >
+        <input name="bot-field"/>
       </p>
-      <StyledInput name="name" placeholder="imię" type="text" />
+      <label className={style.styledLabel}>Imię</label>
+      <input
+        className={style.styledImput}
+        name="name"
+        placeholder="imię"
+        type="text"
+      />
 
-      {/* <StyledLabel>Imię</StyledLabel>
+      {/* 
       <StyledInput input type="text" name="name"></StyledInput>
       <StyledLabel>Email</StyledLabel>
       <StyledInput input type="email" name="email"></StyledInput>
